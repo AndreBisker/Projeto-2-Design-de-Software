ef30eb0f247eb4b27aaaaa38981cde5173c04aa9
Andre Bisker <andrekb@al.insper.edu.br>
2026-04-08 20:32:13 -0300

    guardar_dado

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,12 @@ def rolar_dados(x):
     for i in range(x):
         valores.append(random.randint(1,6))
     return valores
+def guardar_dado(r,g,n):
+    g.append(r[n])
+    r.pop(n)
+    return [r,g]
+dados_rolados = [1, 3, 2]
+dados_no_estoque = [1, 2]
+dado_para_guardar = 1
+
+print(guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar))
