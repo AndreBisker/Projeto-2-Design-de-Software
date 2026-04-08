d8c341eafb2e80f3cbd2a6a092617fcd12374f95
Andre Bisker <andrekb@al.insper.edu.br>
2026-04-08 20:40:52 -0300

    guardar dado

--- funcoes.py
+++ funcoes.py
@@ -9,8 +9,3 @@ def guardar_dado(r,g,n):
     g.append(r[n])
     r.pop(n)
     return [r,g]
-dados_rolados = [1, 3, 2]
-dados_no_estoque = [1, 2]
-dado_para_guardar = 1
-
-print(guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar))
