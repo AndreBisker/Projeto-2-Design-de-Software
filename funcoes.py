2a7410eb239d52cb01528970ddb1fc0908b7fb43
Andre Bisker <andrekb@al.insper.edu.br>
2026-04-08 21:16:10 -0300

    calcula pontos

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,19 @@ def remover_dado(r,g,n):
     r.append(g[n])
     g.pop(n)
     return [r,g]
+def calcula_pontos_regra_simples(f):
+    d={1:0,2:0,3:0,4:0,5:0,6:0}
+    for i in range(len(f)):
+        if f[i]==1:
+            d[1]+=1
+        elif f[i]==2:
+            d[2]+=2
+        elif f[i]==3:
+            d[3]+=3
+        elif f[i]==4:
+            d[4]+=4
+        elif f[i]==5:
+            d[5]+=5
+        elif f[i]==6:
+            d[6]+=6
+    return d
